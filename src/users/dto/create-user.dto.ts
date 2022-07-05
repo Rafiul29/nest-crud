f7e851099dd0757bd88cc5da697048ec7d8e74b5
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-05 22:02:04 +0600

    data validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+}
+
